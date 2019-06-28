19640614c7a60cd9590fdcdec6317b45b67f90dc
友塚　正樹 <uu144692@CA4167.local>
2019-06-28 16:41:44 +0900

    feature: remove css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import Vue from 'vue';
 import App from './components/App';
 
-import './css/style.css';
-
 const app = new Vue({
   render: h => h(App),
 });
 
-app.$mount('#root');
+app.$mount('#app');
